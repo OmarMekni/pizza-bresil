7813ebdff2c896e941126d9c7543c7b98eb5e52b
omar mekni <mekni94@hotmail.com>
2024-04-29 21:44:15 -0400

    ajout balise p dans le fichier index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ export default function Home() {
     <Seo title="Pizza Brésil" description="Application pizza brésil"/>
     <h1>Ciao</h1>
     <h2>Benvenuto</h2>
+    <div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores cum perferendis facere tempore, odio illo quidem labore eligendi nostrum non iusto expedita dicta illum magnam veritatis quos cupiditate, aliquam ex!</p>
+    </div>
     </>
   );
 }

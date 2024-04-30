2c7b4ab7b1875f66757f670d2edb65d84ffc22c5
omar mekni <mekni94@hotmail.com>
2024-04-29 22:04:24 -0400

    ajout du fichier firebase-config.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5gRb-t1d1f9RcgNXQPy-NjTXMVzMpDlY",
+  authDomain: "pizza-bresil.firebaseapp.com",
+  projectId: "pizza-bresil",
+  storageBucket: "pizza-bresil.appspot.com",
+  messagingSenderId: "919749478659",
+  appId: "1:919749478659:web:90e57f237425ba738d0136"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

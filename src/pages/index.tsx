6b2168ce3f0214edc55b6f9a46cde52597012299
omar <omar@gmail.com>
2024-04-29 21:19:20 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+
+import { Inter } from "next/font/google";
+import { Seo } from "@/ui/components/seo";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+    <Seo title="Pizza Brésil" description="Application pizza brésil"/>
+    <h1>Ciao</h1>
+    </>
+  );
+}
